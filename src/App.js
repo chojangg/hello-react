e41130d40f782d2c3db23c881e200628a1c886f3
DESKTOP-0PEQEFP\JAYUN <s2115@e-mirim.hs.kr>
2022-09-24 12:31:26 +0900

    오늘의 체크인 기록

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import CheckInRecoed from "./CheckInRecord";
+import Student from "./Student";
+import './App.css'
 
 function App() {
-  const name = "Jada";
-  const element = <h1>My name is {name}!!</h1>
-  const element2 = <h1>1 + 1 = {1+1}</h1>
+  const name = "Alicia";
+  // const element = <h1>My name is {name}!!</h1>
+  // const element2 = <h1>1 + 1 = {1+1}</h1>
   return (
     <div>
-      <h1>Hello, world!</h1>
+      {/* <h1>Hello, world!</h1>
       {element}
-      {element2}
+      {element2} */}
+      <h1 id="title1" className="title">오늘의 체크인 기록</h1>
+      <CheckInRecoed name="최자윤" score={2} comment="피곤"></CheckInRecoed>
+      <CheckInRecoed name="alicia" score={5} comment="hi"/>
+      <h1 className="title">학생 목록</h1>
+      <Student name="Alicia1"/>
+      <Student name="Alicia2"/>
+      <h1 className="title">제목3</h1>
+      <h1>제목4</h1>
+      <h1>제목5</h1>
     </div>
   );
 }
---
+++ src/Student.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+class Student extends React.Component {
+    constructor (props) {
+        super(props)
+        this.state = {
+            score: 0
+        }
+
+        this.minusScore = this.minusScore.bind(this)
+        this.plusScore = this.plusScore.bind(this)
+    }
+    
+    minusScore() {
+        this.setState({score: this.state.score - 1})
+    }
+
+    plusScore() {
+        this.setState({score: this.state.score + 1})
+    }
+
+    render() {
+        return(
+            <div>
+                이름: {this.props.name}
+                <br/>
+                점수: {this.state.score}
+                {' '}
+                <button type="button" onClick={this.minusScore}>-</button>
+                <button type="button" onClick={this.plusScore}>+</button>
+            </div>
+        );
+    }
+}
+
+export default Student;
